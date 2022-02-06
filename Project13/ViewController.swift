0a68e91325bc061560d8e63f4084dd2e8b27178b
Robin Phillips <robinlmp@mac.com>
2022-02-06 15:08:36 +0000

    Final part of Day 51 still to complete

--- Project13/ViewController.swift
+++ Project13/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func saveButton(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+    
 }
 
