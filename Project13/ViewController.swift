ee953f4bfa72b89c627f8862519f7643aba22fb6
Robin Phillips <robinlmp@mac.com>
2022-02-07 10:00:20 +0000

    Challenge 1 complete

--- Project13/ViewController.swift
+++ Project13/ViewController.swift
@@ -100,7 +100,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
     }
     
     @IBAction func saveButton(_ sender: Any) {
-        guard let image = imageView.image else { return }
+        guard let image = imageView.image else {
+            let ac = UIAlertController(title: "There's no image to save!", message: nil, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+            return
+        }
 
             UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
     }
